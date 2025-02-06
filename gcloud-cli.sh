97f8c176a03bb82015709916b68d506bfba09d93
Giridhar Vedula <giridharvedula@vgs.local>
2025-02-06 14:19:39 +0530

    added first file

---
+++ gcloud-cli.sh
@@ -0,0 +1,11 @@
+# list the active account name
+gcloud auth list 
+
+# list the project
+gcloud config list project 
+
+# set the given region
+gcloud config set compute/region “REGION”
+
+# set the given zone 
+gcloud config set compute/zone “ZONE” 
