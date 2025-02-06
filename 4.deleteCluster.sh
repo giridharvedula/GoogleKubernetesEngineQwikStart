9def71998dbafb76c424767b6f0566910c3f3947
Giridhar Vedula <giridharvedula@gmail.com>
2025-02-06 15:06:44 +0530

    3

---
+++ 4.deleteCluster.sh
@@ -0,0 +1,8 @@
+# below gcloud cli will delete the cluster
+gcloud container clusters delete ClusterName
+
+# below kubectl command will delete the deployment
+kubectl delete -f deployment.yaml
+
+# to delete using the deployment name
+kubectl delete deployment DeploymentName 
